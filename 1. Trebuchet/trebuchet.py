40fe60c9e50cff57a0beb5523d4a9f8a5b1cac3e
Jamie Creus <jamie.creus@derivco.co.uk>
2023-12-05 14:36:21 +0000

    Reverted change to List Comprehension. More complex logic required in part 2

--- 1. Trebuchet/trebuchet.py
+++ 1. Trebuchet/trebuchet.py
@@ -3,13 +3,21 @@ import re
 def readCalibrationValuesFromFile(inputFilePath):
   
   with open(inputFilePath, "r") as inputFile:
+
+    for inputLine in inputFile:
     
-    calibrationValues = [
-      int(str(digitsInLine[0]) + str(digitsInLine[-1]))
-      for inputLine in inputFile
-      if (digitsInLine := re.findall("[0-9]", inputLine))
-    ]
-   
+      digitsInLine = re.findall("[0-9]", inputLine)
+      
+      if len(digitsInLine) == 0:
+        continue
+
+      firstValue = digitsInLine[0]
+      lastValue = digitsInLine[-1]
+      concatenatedValues = str(firstValue) + str(lastValue)
+
+      calibrationValue = int(concatenatedValues)
+      calibrationValues.append(calibrationValue)
+
   return calibrationValues
 
 calibrationValues = readCalibrationValuesFromFile("1. Trebuchet\input.txt")
