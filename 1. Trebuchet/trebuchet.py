bde9e56e40aca6ff2fd8d5f2491ea70b6f86ba13
Jamie Creus <jamie.creus@derivco.co.uk>
2023-12-05 14:33:58 +0000

    Update readCalibrationValuesFromFile to use List Comprehension

--- 1. Trebuchet/trebuchet.py
+++ 1. Trebuchet/trebuchet.py
@@ -1,24 +1,15 @@
 import re
 
 def readCalibrationValuesFromFile(inputFilePath):
-  calibrationValues = []
-
+  
   with open(inputFilePath, "r") as inputFile:
-
-    for inputLine in inputFile:
     
-      digitsInLine = re.findall("[0-9]", inputLine)
-      
-      if len(digitsInLine) == 0:
-        continue
-
-      firstValue = digitsInLine[0]
-      lastValue = digitsInLine[-1]
-      concatenatedValues = str(firstValue) + str(lastValue)
-
-      calibrationValue = int(concatenatedValues)
-      calibrationValues.append(calibrationValue)
-
+    calibrationValues = [
+      int(str(digitsInLine[0]) + str(digitsInLine[-1]))
+      for inputLine in inputFile
+      if (digitsInLine := re.findall("[0-9]", inputLine))
+    ]
+   
   return calibrationValues
 
 calibrationValues = readCalibrationValuesFromFile("1. Trebuchet\input.txt")
